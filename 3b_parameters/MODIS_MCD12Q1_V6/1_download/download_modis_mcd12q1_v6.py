f37ab6c2c9328d61e0275f93a67dd9c7e4e7c423
DaveCasson <davecasson@gmail.com>
2021-05-09 19:21:49 -0600

    Addition of setup.py, utility functions and modis download script update
    This commit contains:
    - The addition of a setup.py to allow local modules to be imported from the utility scripts folder. This has to be run with a command like "pip install ." or pip install -e {local_dir}
    - Addition of a utility_scripts directory containing helper functions, and configuration for a logger.
    - Updated download_modis script that uses both the utility functions and threading with additional checks.

--- 3b_parameters/MODIS_MCD12Q1_V6/1_download/download_modis_mcd12q1_v6.py
+++ 3b_parameters/MODIS_MCD12Q1_V6/1_download/download_modis_mcd12q1_v6.py
@@ -1,9 +1,17 @@
-# Download MODIS MCD12Q1_V6
-# Script based on example provided on: https://git.earthdata.nasa.gov/projects/LPDUR/repos/daac_data_download_python/browse
-# Requires a `.netrc` file in user's home directory with login credentials for `urs.earthdata.nasa.gov`. See: https://lpdaac.usgs.gov/resources/e-learning/how-access-lp-daac-data-command-line/
+"""
 
-# modules
-import os
+Download MODIS MCD12Q1_V6
+
+This script downloads all required MODIS data, approximately 5669 files with a total of 24.67 GB
+Based on example provided on: https://git.earthdata.nasa.gov/projects/LPDUR/repos/daac_data_download_python/browse
+
+***NOTE***
+Requires a `.netrc` file in user's home directory with login credentials for `urs.earthdata.nasa.gov`.
+See: https://lpdaac.usgs.gov/resources/e-learning/how-access-lp-daac-data-command-line/
+"""
+
+# Import Modules
+import os,sys,glob
 import time
 import shutil
 import requests
@@ -12,8 +20,11 @@ from concurrent.futures.thread import ThreadPoolExecutor
 from netrc import netrc
 from pathlib import Path
 from shutil import copyfile
-from datetime import datetime
+import subprocess
 
+#Import local modules
+from workflow_utility_functions import read_from_control,make_default_path, create_log_file
+thisFile = os.path.basename(sys.argv[0])
 
 # --- Control file handling
 # Easy access to control file folder
@@ -22,53 +33,67 @@ controlFolder = Path('../../../0_control_files')
 # Store the name of the 'active' file in a variable
 controlFile = 'control_active.txt'
 
-# Function to extract a given setting from the control file
-def read_from_control( file, setting ):
-    
-    # Open 'control_active.txt' and ...
-    with open(file) as contents:
-        for line in contents:
-            
-            # ... find the line with the requested setting
-            if setting in line:
-                break
-    
-    # Extract the setting's value
-    substring = line.split('|',1)[1]      # Remove the setting's name (split into 2 based on '|', keep only 2nd part)
-    substring = substring.split('#',1)[0] # Remove comments, does nothing if no '#' is found
-    substring = substring.strip()         # Remove leading and trailing whitespace, tabs, newlines
-       
-    # Return this value    
-    return substring
-    
-# Function to specify a default path
-def make_default_path(suffix):
-    
-    # Get the root path
-    rootPath = Path( read_from_control(controlFolder/controlFile,'root_path') )
-    
-    # Get the domain folder
-    domainName = read_from_control(controlFolder/controlFile,'domain_name')
-    domainFolder = 'domain_' + domainName
-    
-    # Specify the forcing path
-    defaultPath = rootPath / domainFolder / suffix
-    
-    return defaultPath
-
-#Function to request and download data
 def request_get(file_url,output_file, usr, pwd):
-    res = requests.get(file_url, verify=True, stream=True, auth=(usr, pwd))
+    '''Function to request and download data, given user credentials'''
 
-    # Decode the response
-    res.raw.decode_content = True
-    content = res.raw
+    try:
+        res = requests.get(file_url, verify=True, stream=True, auth=(usr, pwd))
 
-    # Write to file
-    with open(output_file, 'wb') as data:
-        shutil.copyfileobj(content, data)
+        # Decode the response
+        res.raw.decode_content = True
+        content = res.raw
+
+        # Write to file
+        with open(output_file, 'wb') as data:
+            shutil.copyfileobj(content, data)
+
+    except:
+        logger.warning(f'File {file_url} was not downloaded correctly, on attempt {retries_cur} of {retires_max}')
+        retries_cur += 1
 
     return None
+
+def run_modis_download(file_list,usr,pwd,modis_path):
+    '''Download the needed files using Threading'''
+
+    with ThreadPoolExecutor() as executor:
+        futures = []
+        for file_url_raw in file_list:
+
+            file_url = file_url_raw.strip()
+            file_name = file_url.split('/')[-1].strip()  # Get the last part of the url, strip whitespace and characters
+
+            #Check if file already exists  and move to next file if so
+            if (modis_path / file_name).is_file():
+                logger.debug(f'File {file_name} exists, skipping download')
+            else:
+                #Set the output file name, and submit the download request
+                output_file = os.path.join(modis_path, file_name)
+                futures.append(executor.submit(request_get, file_url, output_file, usr, pwd))
+
+                logger.info(f'Downloading file: {file_name} from: {file_url}')
+
+    return None
+
+def download_check(file_list, modis_path,retries_cur):
+    '''This function checks that all needed files are downloaded, and if not will try again '''
+
+    check_folder = str(modis_path) + "/*.hdf"
+    file_list_check = glob.glob(check_folder)
+
+    file_list.sort()
+    file_list_check.sort()
+
+    if len(file_list) == len(file_list_check):
+        logger.info(f'All required files have been downloaded')
+        download_complete_bool = True
+    else:
+        logger.warning(f'Required files were not downloaded, another attempt will be made')
+        download_complete_bool = False
+        retries_cur += 1
+
+    return download_complete_bool,retries_cur
+
 # --- Get the download settings
 # Path and name of file with download links
 links_path = read_from_control(controlFolder/controlFile,'parameter_land_list_path')
@@ -85,15 +110,25 @@ modis_path = read_from_control(controlFolder/controlFile,'parameter_land_raw_pat
 
 # Specify the default paths if required 
 if modis_path == 'default':
-    modis_path = make_default_path('parameters/landclass/1_MODIS_raw_data') # outputs a Path()
+    modis_path = make_default_path('parameters/landclass/1_MODIS_raw_data',controlFolder,controlFile) # outputs a Path()
 else:
     modis_path = Path(modis_path) # make sure a user-specified path is a Path()
     
 # Make output dir
 modis_path.mkdir(parents=True, exist_ok=True)
 
+# Set the log path and file name
+logPath = modis_path
+log_suffix = '_modis_download_'
 
-# --- Get the authentication info
+# Create a log folder
+logFolder = '_workflow_log'
+Path(logPath / logFolder).mkdir(parents=True, exist_ok=True)
+
+#Create a logging file
+logger = create_log_file(logPath / logFolder,thisFile,suffix=log_suffix)
+
+# Get the authentication info
 # authentication url
 url = 'urs.earthdata.nasa.gov'
 
@@ -108,42 +143,40 @@ pwd = netrc(netrc_folder).authenticators(url)[2]
 # Get the download links from file
 file_list = open(links_file, 'r').readlines()
 
-# Download needed files with threading
-with ThreadPoolExecutor() as executor:
-    futures = []
-    for file_url_raw in file_list:
+logger.info('Downloading MODIS MCD12Q1_V6 data with global coverage.')
 
-        file_url = file_url_raw.strip()
+check_folder = str(modis_path)+"/*.hdf"
+file_list_check = glob.glob(check_folder)
 
-        file_name = file_url.split('/')[-1].strip()  # Get the last part of the url, strip whitespace and characters
-        output_file = os.path.join(modis_path, file_name)
-        print(f'Downloading file: {file_name} from: {file_url}')
-        futures.append(executor.submit(request_get, file_url,output_file, usr, pwd))
+file_list.sort()
+file_list_check.sort()
 
+download_complete_bool = False
+retries_cur = 1
+retries_max = 10
+
+"""This is the main download loop"""
+while download_complete_bool == False:
+
+    #Run download given complete list
+    run_modis_download(file_list, usr, pwd, modis_path)
+    #Check if number of files meets the length of the list
+    download_complete_bool,retries_cur = download_check(file_list, modis_path,retries_cur)
+
+    #Break when all files are downloaded
+    if download_complete_bool == True:
+        break
+
+    #If there are too many retries, then break
+    if retries_cur >= retries_max:
+        logger.error(f'Maximum number of tries ({retries_max}) has been reached, aborting')
+        break
 
 # --- Code provenance
-# Generates a basic log file in the domain folder and copies the control file and itself there.
-
-# Set the log path and file name
-logPath = modis_path
-log_suffix = '_modis_download_log.txt'
-
-# Create a log folder
-logFolder = '_workflow_log'
-Path( logPath / logFolder ).mkdir(parents=True, exist_ok=True)
-
+# Generates copies the control file and itself there.
+#Copy the control file
+copyfile(controlFolder / controlFile, logPath / logFolder / controlFile)
 # Copy this script
-thisFile = 'download_modis_mcd12q1_v6.py'
-copyfile(thisFile, logPath / logFolder / thisFile);
+copyfile(thisFile, logPath / logFolder / thisFile)
 
-# Get current date and time
-now = datetime.now()
 
-# Create a log file 
-logFile = now.strftime('%Y%m%d') + log_suffix
-with open( logPath / logFolder / logFile, 'w') as file:
-    
-    lines = ['Log generated by ' + thisFile + ' on ' + now.strftime('%Y/%m/%d %H:%M:%S') + '\n',
-             'Downloaded MODIS MCD12Q1_V6 data with global coverage.']
-    for txt in lines:
-        file.write(txt) 
---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import pathlib
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+here = pathlib.Path(__file__).parent.resolve()
+
+# Get the long description from the README file
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+
+# Arguments marked as "Required" below must be included for upload to PyPI.
+# Fields marked as "Optional" may be commented out.
+
+setup(
+    name='summaWorkflow_public',  # Required
+    version='0.1',  # Required
+    description='SUMMA workflow repository',  # Optional
+    long_description=long_description,  # Optional
+    long_description_content_type='text/markdown',  # Optional
+    url='https://github.com/CH-Earth/summaWorkflow_public',  # Optional
+    author='Wouter Knoben',  # Optional
+    author_email='Wouter.knoben@usask.ca',  # Optional
+    keywords='hydrology, SUMMA',  # Optional
+
+    # When your source code is in a subdirectory under the project root, e.g.
+    # `src/`, it is necessary to specify the `package_dir` argument.
+    package_dir={'': 'utility_scripts'},  # Optional
+    packages=find_packages(where='utlity_scripts'),  # Required
+
+
+    #python_requires='>=3.6, <4', # Optional
+    #install_requires=requirements # Optional
+
+)
