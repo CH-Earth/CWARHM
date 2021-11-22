d99a118dc8e90bb061b79810376ecf62c2fe94f3
Chris Marsh <chrismarsh.c2@gmail.com>
2021-11-22 16:41:15 -0600

    setup custom rtd script support

--- create_symlinks.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-for x in $(find . -type f -name '*.md' | sort); do name=`echo $x | tr -d \/ | sed "s/^.//g"`;ln -s `pwd`/$x rtd/source/$name;  done
--- rtd/source/create_symlinks.sh
+++ rtd/source/create_symlinks.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-for x in $(find . -type f -name '*.md' | sort); do name=`echo $x | tr -d \/ | sed "s/^.//g"`;ln -s `pwd`/$x rtd/source/$name;  done
+# for x in $(find . -type f -name '*.md' | sort); do name=`echo $x | tr -d \/ | sed "s/^.//g"`;ln -s `pwd`/$x rtd/source/$name;  done
+
+for x in $(find ../.. -type f -name '*.md' | sort); do name=`echo $x | tr -d \/ | sed "s/^.//g" | sed "s/\.\.\.//g"`; ln -s $x $name;  done
