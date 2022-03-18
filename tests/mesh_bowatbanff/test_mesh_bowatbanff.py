1778707354745380696ef83ee781dee0759b1597
bvanosnabrugge <41290330+bvanosnabrugge@users.noreply.github.com>
2022-03-18 15:42:39 -0600

    added some notes to test file

--- tests/mesh_bowatbanff/test_mesh_bowatbanff.py
+++ tests/mesh_bowatbanff/test_mesh_bowatbanff.py
@@ -13,8 +13,6 @@ from cwarhm.model_specific_processing import mesh as mesh
 from cwarhm.model_agnostic_processing import HRU as HRU
 import cwarhm.util.util as utl
 
-
-
 # set paths
 # results_folder_path: path to save the results
 # test_data_path: path with the input test data
@@ -26,6 +24,7 @@ control_options = utl.read_summa_workflow_control_file('control_Bow_at_Banff_tes
 
 
 #%%
+# create mesh topology
 mesh.generate_mesh_topology(control_options['river_network_shp_path'], 
     control_options['river_basin_shp_path'],
     os.path.join(control_options['settings_routing_path'],control_options['settings_routing_topology']),
@@ -33,24 +32,20 @@ mesh.generate_mesh_topology(control_options['river_network_shp_path'],
 ranks, drain_db = mesh.reindex_topology_file(os.path.join(control_options['settings_routing_path'],control_options['settings_routing_topology']))
 drain_db.to_netcdf(os.path.join(control_options['settings_routing_path'],control_options['settings_routing_topology']))
 
-#mesh.hru_zonal_statistics(os.path.join(control_options['parameter_land_mode_path'],control_options['parameter_land_tif_name']),
-#                        os.path.join(control_options['river_basin_shp_path'],control_options['river_basin_shp_name']),
-#                        os.path.join(control_options['settings_routing_path'],control_options['settings_routing_topology']),
-#                        'MESH_parameters.nc')
-
+# calculate land use fractions for MESH GRUS from CWARHM-SUMMA GRU/HRU land use counts
 gdf_land_use_counts = gpd.read_file(os.path.join(
                         control_options['intersect_land_path'],control_options['intersect_land_name']))
-
 df_gru_land_use_fractions = HRU.gru_fraction_from_hru_counts(gdf_land_use_counts)
 
+# set names for discretization
 fraction_type = ['Evergreen Needleleaf Forests','Woody Savannas','Savannas',
 'Grasslands', 'Permanent Wetlands', 'Urban and Built-up Lands', 'Permanent Snow and Ice',
 'Barren','Water Bodies']
-
+# and add fractions and Grouped Response Unit info to the drainage_db
 drain_db = mesh.add_gru_fractions_to_drainage_db(drain_db, df_gru_land_use_fractions, fraction_type)
 drain_db.to_netcdf(os.path.join(control_options['settings_routing_path'],control_options['settings_routing_topology']))
 
-
+# remap forcing data from grids, to MESH GRUs (CWARHM-SUMMA maps to SUMMA HRUs)
 HRU.map_forcing_data(control_options['river_basin_shp_path'],
                     control_options['forcing_merged_path']+'/*200803.nc',
                     control_options['forcing_basin_avg_path']+'/',
